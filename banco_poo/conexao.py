dc636d672566edc845fe91b3473e0201e8f8a8a6
Laercio Gil <laerciogilpb@gmail.com>
2026-04-13 19:12:39 -0300

    feat(dio_blog): add database connection and create clientes table

---
+++ banco_poo/conexao.py
@@ -0,0 +1,14 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS clientes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL)
+""")

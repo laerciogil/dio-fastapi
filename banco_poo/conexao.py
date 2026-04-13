5e2e3eaa6f9e207b8e7976532394147574a50610
Laercio Gil <laerciogilpb@gmail.com>
2026-04-13 19:59:01 -0300

    feat(dio_blog): refactor database operations into functions and add delete functionality

--- banco_poo/conexao.py
+++ banco_poo/conexao.py
@@ -6,14 +6,30 @@ ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
 cursor = conexao.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS clientes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL)
-""")
+def criar_tabela(conexao, cursor):
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clientes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(150) NOT NULL)
+    """)
+
+def inserir_cliente(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?)", data)
+    conexao.commit()
+
+def atualizar_cliente(conexao, cursor, cliente_id, nome, email):
+    data = (nome, email, cliente_id)
+    cursor.execute("UPDATE clientes SET nome = ?, email = ? WHERE id = ?", data)
+    conexao.commit()
+
+def excluir_cliente(conexao, cursor, cliente_id):
+    cursor.execute("DELETE FROM clientes WHERE id = ?", (cliente_id,))
+    conexao.commit()
+
 
 data = ("João", "joao@email.com")
-cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?)", data)
-conexao.commit()
+# inserir_cliente(conexao, cursor, *data)
+atualizar_cliente(conexao, cursor, 1, "João Silva", "joao.silva@email.com")
 conexao.close()

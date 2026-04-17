fd0f05a3fa2b6f6e13b1df5f9eb121202ef552de
Laercio Gil <laerciogilpb@gmail.com>
2026-04-17 02:09:52 -0300

    feat(dio_blog): update PostRequest and PostUpdateRequest models to include content and published_at fields

--- dio_blog/schemas/post.py
+++ dio_blog/schemas/post.py
@@ -3,5 +3,12 @@ from datetime import datetime, UTC
 
 class PostRequest(BaseModel):
     title: str
-    date: datetime = datetime.now(UTC)
+    content: str
+    published_at: datetime | None = None
     published: bool = False
+
+class PostUpdateRequest(BaseModel):
+    title: str | None = None
+    content: str | None = None
+    published_at: datetime | None = None
+    published: bool | None = None

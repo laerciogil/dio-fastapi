d4290eb94628757fd01895c141c047efaa7a5a93
Laercio Gil <laerciogilpb@gmail.com>
2026-04-17 00:57:34 -0300

    feature(dio_blog): reorganize imports and enhance lifespan management for database connection

--- dio_blog/main.py
+++ dio_blog/main.py
@@ -1,6 +1,27 @@
+import sqlalchemy as sa
+import databases
+
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-from controller.post import router as post_router
+from dio_blog.controller.post import router as post_router
 
-app = FastAPI(title="DIO Blog API", version="0.1.0", description="API para gerenciamento de posts do blog da DIO")
+DATABASE_URL = "sqlite:///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+app = FastAPI(title="DIO Blog API", 
+              version="0.1.0", 
+              description="API para gerenciamento de posts do blog da DIO",
+              lifespan=lifespan)
+
 app.include_router(post_router)

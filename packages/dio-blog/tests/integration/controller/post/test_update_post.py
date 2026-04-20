29b30f2aac754efb8e860f4ffae4a87d3b6fb5f5
Laercio Gil <laerciogilpb@gmail.com>
2026-04-20 04:33:31 -0300

    test(dio-blog): add integration tests for updating posts with various scenarios

---
+++ packages/dio-blog/tests/integration/controller/post/test_update_post.py
@@ -0,0 +1,65 @@
+import pytest_asyncio
+from fastapi import status
+from httpx import AsyncClient
+from datetime import datetime
+
+@pytest_asyncio.fixture(autouse=True)
+async def populate_post(db):
+    from dio_blog.schemas.post import PostRequest
+    from dio_blog.service.post import PostService
+
+    service = PostService()
+    await service.create(PostRequest(
+        title="post 1",
+        content="This is the first test post.",
+        published_at=datetime.fromisoformat("2025-06-05T10:15:12.555Z"),
+        published=True
+    ))
+    await service.create(PostRequest(
+        title="post 2",
+        content="This is the second test post.",
+        published=False
+    ))
+    await service.create(PostRequest(
+        title="post 3",
+        content="This is the third test post.",
+        published_at=datetime.fromisoformat("2025-07-01T12:23:44.201Z"),
+        published=True
+    ))
+
+async def test_update_post_success(client: AsyncClient, access_token: str):
+    # given
+    post_id = 1
+    data = {"title": "update title post 1"}
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # when
+    response = await client.put(f"/posts/{post_id}", json=data, headers=headers)
+
+    # then
+    content = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert content["title"] == data["title"]
+
+async def test_update_post_not_authenticated_fail(client: AsyncClient):
+    # given
+    post_id = 1
+    data = {"title": "update title post 1"}
+
+    # when
+    response = await client.put(f"/posts/{post_id}", json=data, headers={})
+
+    # then
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+async def test_update_post_not_found_fail(client: AsyncClient, access_token: str):
+    # given
+    post_id = 99999999
+    data = {"title": "update title post 99999999"}
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # when
+    response = await client.put(f"/posts/{post_id}", json=data, headers=headers)
+
+    # then
+    assert response.status_code == status.HTTP_404_NOT_FOUND
